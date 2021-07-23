f7cbe085e931ecd664ccc9bc6fa6672bfa82e146
regaliv <regaliv@example.com>
2021-07-23 16:59:24 +0300

    lost

--- app/src/main/java/android/example/colordescriptor/MainActivity.java
+++ app/src/main/java/android/example/colordescriptor/MainActivity.java
@@ -3,12 +3,31 @@ package android.example.colordescriptor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColors;
+    private TextView textviewDescriptionTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerColors = findViewById(R.id.spinnerColors);
+        textviewDescriptionTemp = findViewById(R.id.textviewDescriptionTemp);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description = getDescritionByPositiion(position);
+        textviewDescriptionTemp.setText(description);
+    }
+
+    private String getDescritionByPositiion(int position){
+        String[] description = getResources().getStringArray(R.array.description_of_temp);
+        return description[position];
     }
 }
